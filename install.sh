cf621ff65537e6c85a82d78e7e92561e8100dffb
David Senack <david.senack@gmail.com>
2024-06-16 14:06:54 -0400

    rewrite install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
 
-cp -r .* $HOME
+# Backup pre-existing dotfiles
+mv $HOME/.config/dconf/user $HOME/.config/dconf/user.bak
+mv $HOME/.tmux.conf $HOME/.tmux.conf.bak
+mv $HOME/.vimrc $HOME/.vimrc.bak
+mv $HOME/.zshrc $HOME/.zshrc.bak
+
+# Make GNOME-Terminal dconf directory
+mkdir -p $HOME/.config/dconf
+
+# Copy dotfiles to user home directory
+cp .config/dconf/user $HOME/.config/dconf
+cp .tmux.conf $HOME
+cp .vimrc $HOME
+cp .zshrc $HOME
+
+# Reload Zsh
+source .zshrc
