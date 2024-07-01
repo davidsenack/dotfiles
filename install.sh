5cf95ffd8f278c6205933d0dd3809aa8ae4d81ea
David Senack <david.senack@gmail.com>
2024-07-01 08:26:42 -0400

    add font install to install.sh

--- install.sh
+++ install.sh
@@ -1,24 +1,27 @@
 #!/bin/sh
 
-# Point to target snapshot directory
+# Point to target snapshot directory.
 target_dir="./snapshots/default_snapshot"
+
+# Install fonts from fonts directory.
+cp fonts $HOME/.local/share
 
 # Install packages needed
 cat packages.install | xargs sudo apt install -y
 
-# Add zsh install and config
+# Add zsh install and config.
 #::TODO
 
-# Backup pre-existing dotfiles
+# Backup pre-existing dotfiles.
 mv $HOME/.config/dconf/user $HOME/.config/dconf/user.bak
 mv $HOME/.tmux.conf $HOME/.tmux.conf.bak
 mv $HOME/.vimrc $HOME/.vimrc.bak
 mv $HOME/.zshrc $HOME/.zshrc.bak
 
-# Make GNOME-Terminal dconf directory
+# Make GNOME-Terminal dconf directory.
 mkdir -p $HOME/.config/dconf
 
-# Copy dotfiles to user home directory
+# Copy dotfiles to user home directory.
 cp $target_dir/.config/dconf/user $HOME/.config/dconf
 cp $target_dir/packages.install $HOME/.packages.install
 cp $target_dir/.tmux.conf $HOME
