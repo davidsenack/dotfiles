d0858086e2d7dd66bc469ffad2fcc981e16c02d5
David Senack <david.senack@gmail.com>
2024-07-02 20:39:40 -0400

    fix directory paths and recrusive flags to copy commands

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ target_dir="./snapshots/default_snapshot"
 cp fonts $HOME/.local/share
 
 # Install packages needed
-cat packages.install | xargs sudo apt install -y
+cat $target_dir/packages.install | xargs sudo apt install -y
 
 # Add zsh install and config.
 #::TODO
@@ -22,8 +22,8 @@ mv $HOME/.zshrc $HOME/.zshrc.bak
 mkdir -p $HOME/.config/dconf
 
 # Copy dotfiles to user home directory.
-cp $target_dir/.config/dconf/user $HOME/.config/dconf
-cp $target_dir/packages.install $HOME/.packages.install
-cp $target_dir/.tmux.conf $HOME
-cp $target_dir/.vimrc $HOME
-cp $target_dir/.zshrc $HOME
+cp -r $target_dir/.config/dconf/user $HOME/.config/dconf
+cp -r $target_dir/packages.install $HOME/.packages.install
+cp -r $target_dir/.tmux.conf $HOME
+cp -r $target_dir/.vimrc $HOME
+cp -r $target_dir/.zshrc $HOME
